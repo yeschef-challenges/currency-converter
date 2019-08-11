eea224bc1d48fffa634acb64315a0b91fb5a6088
yc-tal <52032669+yc-tal@users.noreply.github.com>
2019-08-11 18:55:54 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+const http = require('http');
+
+const rates = {
+    usd: 1
+};
+
+function setRates(urlParts, dataObj) {
+    //todo: store/update a rate
+}
+
+function convert(requestParams) {
+    //convert between two currencies
+}
+
+http.createServer((req, res) => {
+    const urlParts = req.url.split("/");
+    if (req.method === "GET") {
+        switch (urlParts[1]) {
+            case "convert":
+                try {
+                    res.end(convert(urlParts));
+                } catch (e) {
+                    res.statusCode = 401;
+                    res.end(e.message);
+                }
+                break;
+            default:
+                res.statusCode = 401;
+                res.end("bad request");
+        }
+    } else if (req.method === "POST") {
+        let data = []
+        req.on('data', chunk => {
+            data.push(chunk)
+        });
+        req.on('end', () => {
+            console.log("request data is " + data);
+            const reqDataObj = {};
+            data.join().split("&").forEach(param => {
+                const paramParts = param.split("=");
+                reqDataObj[paramParts[0]] = paramParts[1];
+            });
+            switch (urlParts[1]) {
+                case "rates":
+                    try {
+                        setRates(urlParts, reqDataObj);
+                        res.statusCode = 200;
+                        res.end("Ok");
+                    } catch (e) {
+                        res.statusCode = 401;
+                        res.end(e.message);
+                    }
+
+                    break;
+                default:
+                    res.statusCode = 401;
+                    res.end("bad request");
+            }
+        });
+    } else {
+        res.statusCode = 401;
+        res.end("bad request");
+    }
+    console.log(req.url);
+}).listen(8080);
+
+console.log("server is up and running on port 8080")
